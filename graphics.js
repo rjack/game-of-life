68ac87b104d227fa926e97af8564f2ecd453fcc2
Giacomo Ritucci <giacomo.ritucci@email.it>
2010-09-22 00:40:40 +0200

    GRAPHIC module

--- graphics.js
+++ graphics.js
@@ -1,3 +1,50 @@
+/*
+ * Game of Life Graphics Module
+ *
+ * Draws a WORLD's description on an HTML canvas.
+ */
+
 this.GRAPHICS = (function ()
 {
+	var cnv, ctx,
+
+		cell = {
+			width: 0,
+			height: 0
+		},
+
+
+		init = function (config)
+		{
+			cnv = config.canvas;
+			ctx = cnv.getContext("2d");
+			cell.width = config.cell.width;
+			cell.height = config.cell.height;
+		},
+
+
+		update = function (worldDescription)
+		{
+			var i_width, i_height, grid;
+
+			grid = JSON.parse(worldDescription);
+
+			for (i_width = 0; i_width < grid.length; i_width++) {
+				for (i_height = 0; i_height < grid[i_width].length; i_height++) {
+					if (grid[i_width][i_height]) {
+						ctx.fillStyle = "black";
+					} else {
+						ctx.fillStyle = "white";
+					}
+					ctx.fillRect(i_width * cell.width, i_height * cell.height, cell.width, cell.height);
+				}
+			}
+		};
+
+
+	// GRAPHICS's public interface
+	return {
+		init: init,
+		update: update
+	};
 }());
