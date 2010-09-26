b833052e969c80381d4ce431b6d1ca00dba43a5a
Giacomo Ritucci <giacomo.ritucci@email.it>
2010-09-27 00:40:26 +0200

    physics module stub

---
+++ physics.js
@@ -0,0 +1,21 @@
+this.PHYSICS = (function ()
+{
+	var step,
+
+		init = function (config)
+		{
+			console.log("TODO");
+		},
+
+		next = function (world)
+		{
+			console.log("TODO");
+		};
+
+
+	// PHYSICS' public interface.
+	return {
+		init: init,
+		next: next
+	};
+}());
